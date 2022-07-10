e40855f9566239bbad4fa1d34b6875af3456e458
Dreyf <dreyfcesar@gmail.com>
2022-07-10 14:23:13 -0300

    Crea datos de archivo
    Crea la interfaz y una implementación para datos básicos de un archivo.

---
+++ Datos/Archivos/Archivo.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Mow\Datos\Archivos;
+
+use Mow\Interfaz\Archivos\Archivo as IArchivo;
+
+class Archivo implements IArchivo
+{
+    private $archivo_ruta;
+
+    public function __construct(string $archivo_ruta)
+    {
+        $this->archivo_ruta = $archivo_ruta;
+
+        if( !file_exists($archivo_ruta) ) {
+            trigger_error("El archivo $archivo_ruta no existe", E_USER_ERROR);
+        }
+    }
+
+    public function ruta(): string
+    {
+        return $this->archivo_ruta;
+    }
+
+}
+
+?>
---
+++ Interfaz/Archivos/Archivo.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Mow\Interfaz\Archivos;
+
+interface Archivo extends Ruta
+{
+}
+
+?>
